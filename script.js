9ed40c37d559ccaa394fb5ecaca9445dead34a7d
ItsStunZ <115426921+ItsStunZ@users.noreply.github.com>
2026-03-21 22:59:55 +0000

    Built basic Book constructor

---
+++ script.js
@@ -0,0 +1,13 @@
+const bookLibrary = []; // All books are stored here
+
+
+function Book(title, author, pages, read) {
+    if (!new.target) {
+        throw Error("You must have the 'new' operator before calling Book");
+    }
+
+    this._title = title;
+    this._author = author;
+    this._pages = pages;
+    this._read = read;
+}
