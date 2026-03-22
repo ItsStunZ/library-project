c09a1043b629201a56f7cb18ebe1b77b204fc820
ItsStunZ <115426921+ItsStunZ@users.noreply.github.com>
2026-03-22 02:40:37 +0000

    Created function that displays books on DOM

--- script.js
+++ script.js
@@ -16,3 +16,43 @@ function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     bookLibrary[crypto.randomUUID()] = newBook;
 }
+
+function displayBooks() {
+    const booksContainer = document.querySelector('.books_container');
+    // loop through bookLibrary and display on page
+    for (let id in bookLibrary) {
+        // Create new card
+        const card = document.createElement('div');
+        card.classList.add('.card');
+        
+        // Create title header
+        const titleHeader = document.createElement('h1');
+        titleHeader.textContent = bookLibrary[id]._title;
+        card.appendChild(titleHeader);
+
+        // Create author header
+        const authorHeader = document.createElement('h2');
+        authorHeader.textContent = bookLibrary[id]._author;
+        card.appendChild(authorHeader);
+
+        // Create #pages paragraph
+        const pages = document.createElement('p');
+        pages.textContent = bookLibrary[id]._pages;
+        card.appendChild(pages);
+
+        // Create has read paragraph
+        const hasRead = document.createElement('p');
+        hasRead.textContent = bookLibrary[id]._read ? 'Already read' : 'Not yet read';
+        card.appendChild(hasRead);
+
+        booksContainer.appendChild(card);
+    }
+}
+
+
+// create some books for debugging
+addBookToLibrary('Bossypants', 'Tina Fey', 275, false);
+addBookToLibrary("Are You There, Vodka? It's Me, Chelsea", 'Chelsea Handler', 256, false);
+addBookToLibrary('Calypso', 'David Sedaris', 272, true);
+
+displayBooks();
